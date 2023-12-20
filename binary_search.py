673a860dc5b680e457a1c6fc67546a22a1e426d3
Anastasyia Solovyeva <121310348+FoxyNastya@users.noreply.github.com>
2023-12-20 19:39:10 +0300

    Update binary_search.py ver 2.0

--- binary_search.py
+++ binary_search.py
@@ -1,26 +1,25 @@
- 
-def binary_search(list1, n): 
-    low = 0 
-    high = len(list1) - 1 
-    mid = 0 
- 
-    while low  n: 
-            high = mid - 1 
- 
-        else: 
-            return mid 
-             
-    return -1 
- 
- 
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
 
-list1 = [12, 24, 32, 39, 45, 50, 54] 
-n = 45 
- 
- 
-result = binary_search(list1, n) 
- 
-if result != -1: 
-    print("Element is present at index", str(result)) 
-else: 
-    print("Element is not present in list1") 
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
+
+
+numbers = [1, 2, 3, 4, 5]
+target = 4
+result = binary_search(numbers, target)
+
+if result == -1:
+    print("Элемент не найден")
+else:
+    print(f"Элемент располагается по индексу {result}")
